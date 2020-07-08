dcfbde6c984b9a364ed69dc70c79786a5d49557c
frannyfra <francescadelaurentis1987@gmail.com>
2020-07-08 12:14:52 +0100

    Merge form with postcodeApiRequest

--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -2,42 +2,62 @@ import React, { useState } from "react";
 import checkPostcodes from "../../utilities/postcodesApiRequest";
 
 const UserForm = ({ page, setPage }) => {
-    const [postCode1, setPostCode1] = useState("");
-    const [postCode2, setPostCode2] = useState("");
-    const arr = [postCode1, postCode2]
+  const [postCode1, setPostCode1] = useState("");
+  const [postCode2, setPostCode2] = useState("");
+  const [returnedPostcodes, setReturnedPostcodes] = useState([]);
+
   if (page !== "form") {
     return null;
   }
-  
+
   const formSubmit = async event => {
     event.preventDefault();
 
-    const verifiedPostcodes = await checkPostcodes("n22 5ad", "n16 9lxxxx");
+    const verifiedPostcodes = await checkPostcodes(postCode1, postCode2);
 
     if (verifiedPostcodes.notValidPostcodes.length > 0) {
-      const notValidePostcodeArray = verifiedPostcodes.notValidPostcodes;
-      console.log(notValidePostcodeArray);
-      alert(
-        `this postcode is not valid: ${notValidePostcodeArray.map(
-          el => el.query
-        )}`
+      const notValidPostcodeArray = verifiedPostcodes.notValidPostcodes.map(
+        el => el.query
       );
-    } setPage('locations')
+      console.log(notValidPostcodeArray);
+      if (notValidPostcodeArray.includes(postCode1)) {
+        setPostCode1("");
+      }
+      if (notValidPostcodeArray.includes(postCode2)) {
+        setPostCode2("");
+      }
+
+      return alert(`this postcode is not valid: ${notValidPostcodeArray}`);
+    }
+
+    setReturnedPostcodes(
+      returnedPostcodes.push(verifiedPostcodes.validPostcodes)
+    );
+    setPage("locations");
   };
 
-    return (
-        <>
-        <div>
-            <h1> hello I am the user form </h1>
-            <form onSubmit={formSubmit}>
-                <input type="text" name="PostCodeOne" onChange={e => setPostCode1(e.target.value)} />
-                <input type="text" name="PostCodeTwo" onChange={e => setPostCode2(e.target.value)} />
-                <input type="submit" value="Find Locations" />
-            </form>
-            <h2>Arr: {arr}</h2>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h1> hello I am the user form </h1>
+        <form onSubmit={formSubmit}>
+          <input
+            type="text"
+            value={postCode1}
+            name="PostCodeOne"
+            onChange={e => setPostCode1(e.target.value)}
+          />
+          <input
+            type="text"
+            value={postCode2}
+            name="PostCodeTwo"
+            onChange={e => setPostCode2(e.target.value)}
+          />
+          <input type="submit" value="Find Locations" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default UserForm;
