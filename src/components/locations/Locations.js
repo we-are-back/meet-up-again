91d468c959b5e0607e04508e6f59eeab08e648a3
frannyfra <francescadelaurentis1987@gmail.com>
2020-07-16 12:52:22 +0100

    Create location card

--- src/components/locations/Locations.js
+++ src/components/locations/Locations.js
@@ -1,13 +1,19 @@
 // // component
 import React from "react";
+import LocationItem from "./location-item/LocationItem";
 
 const Locations = ({ page, setPage, locations, setLocations }) => {
   if (page !== "locations") {
     return null;
   }
+  console.log("locations in locationComp", locations);
+  //   console.log(formResult.locations, "form locations");
   return (
     <div>
       <h1>List of locations</h1>
+      {locations.map(location => (
+        <LocationItem location={location} />
+      ))}
       <button onClick={() => setPage("result")}>Results</button>
     </div>
   );
--- src/components/locations/location-item/LocationItem.js
+++ src/components/locations/location-item/LocationItem.js
@@ -1,3 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Locations from "../Locations";
 
-
+const locationItem = ({ location }) => {
+  console.log(location);
+  return (
+    <>
+      <h1>{location.name}</h1>
+    </>
+  );
+};
+export default locationItem;
