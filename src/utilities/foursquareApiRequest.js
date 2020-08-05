fc3583b68df11331a99510c405a8ba7123be55e7
frannyfra <francescadelaurentis1987@gmail.com>
2020-08-05 10:46:18 +0100

    Add radius to venues result

--- src/utilities/foursquareApiRequest.js
+++ src/utilities/foursquareApiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const foursquareApiRequest = async (coordinate1, coordinate2, category) => {
   console.log('in Api request', category)
-  const url = `https://api.foursquare.com/v2/venues/search?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_KEY}&v=20180323&limit=10&ll=${coordinate1},${coordinate2}&categoryId=${category}`;
+  const url = `https://api.foursquare.com/v2/venues/search?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_KEY}&v=20180323&limit=10&ll=${coordinate1},${coordinate2}&categoryId=${category}&radius=1000`;
   const response = await axios.get(url);
   console.log(response.data.response.venues)
   return response.data.response.venues;
